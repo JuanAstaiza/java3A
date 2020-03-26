7edae0be19ecfc192f2461e0820b3a6d9369c4c1
JuanAstaiza <juancarlosastaiza@gmail.com>
2020-03-25 20:24:23 -0500

    Reto terminado Botones Enabled.

---
+++ Ejercicios_en_Clase/Ejercicios_1_Corte/src/practicas/Numeros_1_10_for.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicas;
+
+/**
+ *
+ * @author JUAN CARLOS ASTAIZA
+ */
+public class Numeros_1_10_for {
+      public static void main(String[] args) {
+         //DE 1 AL 10 CON CON CLICLO INICIADO LA VARIABLE INCREMENTO EN 0
+          int i=0; 
+          for (i = 0; i < 10; i++) {
+              System.out.println(i+1);
+          }
+          // DEL 1 AL 10 CON CICLO INICIADO LA VARIABLE INCREMENTADO EN 1
+          System.out.println("*************************************");
+          for (i = 1; i <= 10; i++) {
+              System.out.println(i);
+          }
+      }
+}
