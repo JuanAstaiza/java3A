21ec088e337cfc66ebe86d65e6b039660d00fedf
JuanAstaiza <juancarlosastaiza@gmail.com>
2020-04-02 18:33:31 -0500

    Taller práctico Nro 2- JAVA (TERMINADO). StringManager

--- Aplicaciones_Graficas_JAVA/aplicaciones_1Corte/src/ejercicios/Cadenas1.java
+++ Aplicaciones_Graficas_JAVA/aplicaciones_1Corte/src/ejercicios/Cadenas1.java
@@ -14,13 +14,13 @@ import javax.swing.border.TitledBorder;
  *
  * @author JUAN CARLOS ASTAIZA
  */
-public class StringManager extends javax.swing.JFrame {
+public class Cadenas1 extends javax.swing.JFrame {
         String yName="",yNameU=""; //yNameU variable nombre unido.
         int NCharacters=0;
     /**
      * Creates new form StringManager
      */
-    public StringManager() {
+    public Cadenas1() {
         initComponents();
         //Centramos el JFrame en nuestra Pantalla
         this.setLocationRelativeTo(null);
@@ -71,7 +71,6 @@ public class StringManager extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("String Manager");
-        setPreferredSize(new java.awt.Dimension(360, 600));
         setResizable(false);
 
         jPanelPrincipal.setBackground(new java.awt.Color(255, 255, 153));
@@ -355,7 +354,7 @@ public class StringManager extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPInformation, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+            .addComponent(jPInformation, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 0, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -543,20 +542,21 @@ public class StringManager extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new StringManager().setVisible(true);
+                new Cadenas1().setVisible(true);
             }
         });
     }
